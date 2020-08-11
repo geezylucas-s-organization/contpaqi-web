adb7d540126217cb83571d391e6fd779a5cdce53
geezylucas <sorec@live.com>
2020-08-11 03:23:44 -0500

    rtk succesfully

--- src/index.js
+++ src/index.js
@@ -1,21 +1,25 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import { BrowserRouter } from "react-router-dom";
+import { Provider } from "react-redux";
 import CssBaseline from "@material-ui/core/CssBaseline";
 import { ThemeProvider } from "@material-ui/core/styles";
 import App from "./App";
+import store from "./store";
 import theme from "./theme";
 import * as serviceWorker from "./serviceWorker";
 
 ReactDOM.render(
   <React.StrictMode>
-    <ThemeProvider theme={theme}>
-      {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
-      <CssBaseline />
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
-    </ThemeProvider>
+    <Provider store={store}>
+      <ThemeProvider theme={theme}>
+        {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
+        <CssBaseline />
+        <BrowserRouter>
+          <App />
+        </BrowserRouter>
+      </ThemeProvider>
+    </Provider>
   </React.StrictMode>,
   document.getElementById("root")
 );
--- src/screens/home/index.js
+++ src/screens/home/index.js
@@ -2,15 +2,29 @@ import React from "react";
 import Container from "@material-ui/core/Container";
 import Typography from "@material-ui/core/Typography";
 import Box from "@material-ui/core/Box";
+import { increment, decrement } from "../../store/counterSlice";
+import { connect } from "react-redux";
+import { Button } from "@material-ui/core";
 
-const Home = () => (
-  <Container maxWidth="sm">
-    <Box my={4}>
-      <Typography variant="h4" component="h1" gutterBottom>
-        Hello world
-      </Typography>
-    </Box>
-  </Container>
-);
+const Home = ({ counter, increment, decrement }) => {
+  return (
+    <Container maxWidth="lg">
+      <Box my={4}>
+        <Typography variant="h4" component="h1" gutterBottom>
+          Hello world {counter}
+        </Typography>
+        <Button color="primary" onClick={increment}>
+          +
+        </Button>
+        <Button color="primary" onClick={decrement}>
+          -
+        </Button>
+      </Box>
+    </Container>
+  );
+};
 
-export default Home;
+const mapStateToProps = (state) => ({ counter: state.counter });
+const mapDispatchToProps = { increment, decrement };
+
+export default connect(mapStateToProps, mapDispatchToProps)(Home);
