07bd9412528a550a01e59df8c156be59ff02e3e5
unknown <mike__221@hotmail.com>
2020-09-19 01:35:24 -0500

    Change title for PROSIS

--- src/components/Copyright.js
+++ src/components/Copyright.js
@@ -6,7 +6,7 @@ const Copyright = () => (
     <Typography variant="body2" color="textSecondary" align="center">
       {"Copyright © "}
       <Link color="inherit" href="https://material-ui.com/">
-        Your Company
+        PROSIS
       </Link>{" "}
       {new Date().getFullYear()}
       {"."}
